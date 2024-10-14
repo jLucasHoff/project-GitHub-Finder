c5f0b6a5d22573380c8793c596ce1cd8d21bf7f4
João Lucas Hoffman Gonçalves Costa <contato.jlhoffman@gmail.com>
2024-10-14 19:26:18 -0300

    Import useState in Home.tsx

--- project/src/routes/Home.tsx
+++ project/src/routes/Home.tsx
@@ -1,6 +1,9 @@
 import Search from "../components/Search"
 
+import { useState } from "react"
 const Home = () => {
+
+    const [user, setUser] = useState(null)
     return <div>
         <Search />
     </div>
