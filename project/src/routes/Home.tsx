1192c916e95459993169584542d727a22636aba0
João Lucas Hoffman Gonçalves Costa <contato.jlhoffman@gmail.com>
2024-10-14 19:29:47 -0300

    Create loadUser function in Home.tsx

--- project/src/routes/Home.tsx
+++ project/src/routes/Home.tsx
@@ -1,9 +1,15 @@
 import Search from "../components/Search"
+import { UserProps } from "../types/user"
 
 import { useState } from "react"
 const Home = () => {
+    const [user, setUser] = useState<UserProps | null>(null)
+    const loadUser = async(userName: string) => {
+        const res = await fetch(`https://api.github.com/users/${userName}`)
+        const data = await res.json()
+        console.log(data)
+    }
 
-    const [user, setUser] = useState(null)
     return <div>
         <Search />
     </div>
