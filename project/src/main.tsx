81a0b465dc6cb086919d8170c09631a5e5813dae
João Lucas Hoffman Gonçalves Costa <contato.jlhoffman@gmail.com>
2024-10-14 19:17:25 -0300

    Create router object in main.tsx

--- project/src/main.tsx
+++ project/src/main.tsx
@@ -6,6 +6,19 @@ import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from "./routes/Home.tsx"
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      }
+    ]
+  }
+])
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
