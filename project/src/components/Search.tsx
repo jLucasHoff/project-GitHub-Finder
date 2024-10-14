fc1bcfb7d6df95222428a11f095e8c685359355c
João Lucas Hoffman Gonçalves Costa <contato.jlhoffman@gmail.com>
2024-10-14 19:35:17 -0300

    Update Search.tsx

--- project/src/components/Search.tsx
+++ project/src/components/Search.tsx
@@ -2,16 +2,19 @@ type SearchProps = [
     loadUser: (userName: string) => Promise<void>
 ]
 
+import { useState } from "react";
+
 import { BsSearch } from "react-icons/bs";
 
 const Search = ({loadUser}: SearchProps) => {
+    const [userName, setUserName] = useState("")
     return (
         <div>
             <h2>Busque por um usuário:</h2>
             <p>Conheça seus melhores repositórios</p>
             <div>
-                <input type="text" placeholder="Digite o nome do seu usuário" />
-                <button>
+                <input type="text" placeholder="Digite o nome do seu usuário" onChange={(e) => setUserName(e.target.value)}/>
+                <button onClick={() => loadUser(userName)}>
                     <BsSearch />
                 </button>
             </div>
