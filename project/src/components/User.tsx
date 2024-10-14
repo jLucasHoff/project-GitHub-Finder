cf297ee2e1c7344aff7624c94b4cf936c2a0fc24
João Lucas Hoffman Gonçalves Costa <contato.jlhoffman@gmail.com>
2024-10-14 20:11:27 -0300

    Set Error component and style user content

--- project/src/components/User.tsx
+++ project/src/components/User.tsx
@@ -1,6 +1,7 @@
 import { UserProps } from "../types/user"
 import { MdLocationPin } from "react-icons/md"
 import { Link } from "react-router-dom"
+import classes from "./User.module.css"
 
 const User = (
     {
@@ -11,23 +12,26 @@ const User = (
         followers
     }: UserProps) => {
     return (
-        <div>
+        <div className={classes.user}>
             <img src={avatar_url} alt={login} />
             <h2>{login}</h2>
             {location && (
-            <p>
+            <p className={classes.location}>
                 <MdLocationPin />
                 <span>{location}</span>
             </p>
             )}
-            <div>
-                <p>Seguidores:</p>
-                <p>{followers}</p>
+            <div className={classes.stats}>
+                <div>
+                    <p>Seguidores:</p>
+                    <p className={classes.number}>{followers}</p>
+                </div>
+                <div>
+                    <p>Seguindo:</p>
+                    <p className={classes.number}>{following}</p>
+                </div>
             </div>
-            <div>
-                <p>Seguindo:</p>
-                <p>{following}</p>
-            </div>
+            
             <Link to={`/repos/${login}`}>Ver melhores projetos</Link>
 
         </div>
