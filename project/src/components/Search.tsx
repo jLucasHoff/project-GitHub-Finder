2c19c2707347f07f9fb68f456640e3959f75b7a3
João Lucas Hoffman Gonçalves Costa <contato.jlhoffman@gmail.com>
2024-10-14 19:22:19 -0300

    Update Search.tsx

---
+++ project/src/components/Search.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Search = () => {
+    return (
+        <div>
+            <h2>Busque por um usuário:</h2>
+            <p>Conheça seus melhores repositórios</p>
+            <div>
+                <input type="text" placeholder="Digite o nome do seu usuário" />
+                <button></button>
+            </div>
+        </div>
+    )
+}
+
+export default Search;
